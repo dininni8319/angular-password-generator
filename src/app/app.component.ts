1667689a5c4e9d7d3a65d781369855787d4a6b3e
Salvatore Dininni <72815104+dininni8319@users.noreply.github.com>
2023-12-04 18:33:55 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'password-generator';
+  title = 'Angular Material Starter';
+  length = ''
 }
